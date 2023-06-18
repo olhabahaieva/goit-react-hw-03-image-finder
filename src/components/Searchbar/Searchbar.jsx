2b4a542e9f9d5ef295fb134a2f06caf38ac37ba2
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-06-18 20:30:53 +0300

    api request as a sepatate file

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import css from './Searchbar.module.css';
-import axios from 'axios';
+import getImages from 'getImages';
 
 class Searchbar extends Component {
   
@@ -8,20 +8,6 @@ class Searchbar extends Component {
     e.preventDefault();
     const form = e.target;
     const input = form.search.value;
-
-    async function getImages(images, page = 1) {
-      const BASE_URL = 'https://pixabay.com/api/';
-      const API_KEY = '35890843-7500688730c28920b4cfb1288';
-      try {
-        const response = await axios.get(
-          `${BASE_URL}?key=${API_KEY}&q=${images}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
-        );
-        console.log(response);
-        return response.data;
-      } catch (error) {
-        console.log(error);
-      }
-    }
 
     getImages(input);
   };
---
+++ src/getImages.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+async function getImages(images, page = 1) {
+    const BASE_URL = 'https://pixabay.com/api/';
+    const API_KEY = '35890843-7500688730c28920b4cfb1288';
+    try {
+      const response = await axios.get(
+        `${BASE_URL}?key=${API_KEY}&q=${images}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+      );
+      console.log(response);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  export default getImages;
