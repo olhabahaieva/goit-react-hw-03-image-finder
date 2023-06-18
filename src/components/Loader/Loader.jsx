fa3ca515b5e5529cd3f9890a01edc6b39201b0af
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-06-18 19:11:27 +0300

    added loader component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import css from './ImageGalleryItem.module.css';
+
+class Loader extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Loader;
