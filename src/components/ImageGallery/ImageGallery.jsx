40c95a58dae7ae9cd4b31a9ca603b66a5e4f221b
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-06-23 01:45:09 +0300

    added consoles

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,11 +3,14 @@ import css from './ImageGallery.module.css';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 
 const ImageGallery = ({ images }) => {
+  console.log(images)
   if (images.length > 0) {
+    console.log(images)
     return (
       <ul className={css.gallery}>
         {images.map(({ image }) => (
-          <ImageGalleryItem key={image.id} image={image} />
+         
+          <ImageGalleryItem key={image.id} url={image.previewURL} image={image} />
         ))}
       </ul>
     );
