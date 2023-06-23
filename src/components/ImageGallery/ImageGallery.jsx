97cee7d10e57c92d0dda589cb2145887a64592db
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-06-23 16:27:47 +0300

    image gallery response works

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,20 +3,16 @@ import css from './ImageGallery.module.css';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 
 const ImageGallery = ({ images }) => {
-  console.log(images)
-  if (images.length > 0) {
-    console.log(images)
-    return (
-      <ul className={css.gallery}>
-        {images.map(({ image }) => (
+  console.log(images.hits);
+  if (images.hits) {
+    const elements = images.hits.map((el) => (
+      <ImageGalleryItem key={el.id} url={el.previewURL} image={el} />
+    ));
 
-          <ImageGalleryItem key={image.id} url={image.previewURL} image={image} />
-        ))}
-      </ul>
-    );
-  } else {
-    return null;
+    return <ul className={css.gallery}>{elements}</ul>;
   }
+
+  return null; // Return null or a fallback component if `images.hits` is falsy
 };
 
 export default ImageGallery;
