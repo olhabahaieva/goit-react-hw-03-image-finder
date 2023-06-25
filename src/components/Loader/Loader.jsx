26c03d254a456b3090c09dcf3af9fc518af99438
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-06-26 00:10:12 +0300

    spinner alignment is added

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,24 +1,21 @@
-import { Component } from 'react';
 import { Oval } from 'react-loader-spinner';
+import css from './Loader.module.css'
 
-class Loader extends Component {
- 
-  render() {
-    return (
+const Loader = () => {
+  return (
+    <div className={css.loaderContainer}>
       <Oval
         height={100}
         width={100}
         color="#4fa94d"
-        wrapperStyle={{}}
-        wrapperClass=""
         visible={true}
         ariaLabel="oval-loading"
         secondaryColor="#4fa94d"
         strokeWidth={2}
         strokeWidthSecondary={2}
       />
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Loader;
