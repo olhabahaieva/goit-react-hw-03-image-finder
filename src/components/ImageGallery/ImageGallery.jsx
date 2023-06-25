9d290a463bdebd93c8b114bee693906312576104
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-06-25 17:14:31 +0300

    removed unused console

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,7 +3,6 @@ import css from './ImageGallery.module.css';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 
 const ImageGallery = ({ images }) => {
-  console.log(images.hits);
   if (images.hits) {
     const elements = images.hits.map(el => (
       <ImageGalleryItem key={el.id} url={el.webformatURL} tags={el.tags} />
