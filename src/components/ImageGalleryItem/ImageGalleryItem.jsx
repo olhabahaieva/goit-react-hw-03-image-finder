58ba9efeec2db1744ee66c0dcec381488044b68c
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-06-26 09:06:38 +0300

    cleaned up console

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import css from './ImageGalleryItem.module.css';
 
 const ImageGalleryItem = ( {url, tags} ) => {
-  // console.log(image)
     return (
       <li className={css.galleryItem}>
           <img
